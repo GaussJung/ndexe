2e12f8800b00fa30dc21db536dd0880dffe2c27a
GaussJung <cwjung123@gmail.com>
2021-09-02 13:11:01 +0900

    v1.23

---
+++ public/localJS/mainscript.js
@@ -0,0 +1,14 @@
+console.log("S-110 mainscript V1.0");  
+ 
+alert("TM Init 1.0"); 
+
+$(function() { 
+	 
+    // 기본진행창 
+    $("#btnTest").bind("click", function(event, ui) { 
+          alert("TM2 clickTest"); 
+    }); 
+ 
+    
+});  
+
