4a0ddaaaa6a518861574d300696db60ac03e1723
GaussJung <cwjung123@gmail.com>
2021-09-15 08:59:19 +0900

    v2.1 auth

--- public/localJS/homescript.js
+++ public/localJS/homescript.js
@@ -1,6 +1,79 @@
 console.log("S-110 homescript V1.1");  
   
 $(function() { 
+  
+
+    // S1. 사용자 확인 
+    function checkValidUser() { 
+
+        let tmpPwd; 
+        let inPwd; 
+ 
+        let targetURL = "https://adm.soystudy.com/comm/auth/authKeyHandler.jsp";  
+ 
+        $.ajax({
+            url: targetURL,
+            type:'post',
+            dataType: 'json',
+            cache: false,
+            async: true,
+            data: {
+                chcode:'getTempPwd',
+                mcd:tbmcd
+            },
+            success: function(fdata) {
+
+                if ( fdata.cupd === true ) {
+                    
+                    tmpPwd = fdata.tmpPwd; 
+
+                    inPwd =  prompt("비밀번호를 입력해 주세요!","정보입력"); 
+
+                    if ( inPWd == tmpPwd ) {
+                        sessionStorage.setItem("tempAuthValid","PASS"); 
+                        $(".viewBoxWrap").show(); 
+                    }
+                    else {
+                        sessionStorage.removeItem("tempAuthValid"); 
+                        alert("비밀번호가 일치하지 않습니다!");
+                        location.reload();  
+                    }; 
+
+                }
+                else {
+                    alert("접속불가 합니다! 관리자에게 문의바랍니다.");
+                    location.reload();  
+                };
+            },
+            error: function(xhr, errorString, exception) {   // 에러시에 출력
+                // console.log(xhr.responseText);
+                // console.log("xhr.status = " + xhr.status+" / errorString = " + errorString+" / exception = " + exception);
+            }
+    
+        });  
+        // EOF T1-B 
+ 
+    }; 
+
+
+
+
+    // S2. 초기환경 
+    function initEnvSet() { 
+
+        $(".viewBoxWrap").hide(); 
+
+
+        if ( (typeof sessionStorage.getItem("tempAuthValid") ) !== 'undefined' && sessionStorage.getItem("tempAuthValid") !== null ) {
+            // 인증완료상태  
+        }
+        else {
+            checkValidUser(); 
+        }; 
+    }; 
+
+    // 초기접속환경 확인 
+    initEnvSet(); 
  
 
     // P2-1. 홈접속
