6dfb80a230f30a1ac66caf922c273ff51d7e335f
ColinJung <master@entercube.net>
2022-11-03 22:00:00 +0900

    pm2 logs

--- server8000.js
+++ server8000.js
@@ -26,49 +26,42 @@ app.use(express.static('public'));
 // ps.1 시작 페이지
 app.get('/', (req, res) => {
 
-  // 접속시간 정보 설정 
-  let todayDate = new Date(); 
-  let currTime = todayDate.toFormat('YYYY-MM-DD HH24:MI:SS');
+    // 접속시간 정보 설정 
+    let todayDate = new Date(); 
+    let currTime = todayDate.toFormat('YYYY-MM-DD HH24:MI:SS');
 
-  // 접속횟수 추가 
-  totalConnectCnt++; 
+    // 접속횟수 추가 
+    totalConnectCnt++; 
+    
+    let {userId, userName} = req.query;
  
-  // 인자값 전달유무에 따른 페이지 분리 
-  if ( req.query !== undefined  ) {
-      // 인자있음 
-      // 호출사례  http://myserver.com:3000/?userId=1001&userName=James
-      let {userId, userName} = req.query;
- 
-      console.log("C1-0=" + req.query); 
+    // 인자값 전달유무에 따른 페이지 분리 
+    if ( usedId !== undefinded && userName !== undefined ) {
+        // 호출사례  http://myserver.com:3000/?userId=1001&userName=James
+        res.render("main8010", {
+            title: "Member Home",
+            ctime: currTime,
+            totalcnt : totalConnectCnt,
+            uid : userId,
+            uname : userName,
+        });
 
-      res.render("main8010", {
-          title: "Member Home",
-          ctime: currTime,
-          totalcnt : totalConnectCnt,
-          uid : userId,
-          uname : userName,
-      });
+        console.log("C1-A. Service WebPage HOME Time=" + currTime + " / Count=" + totalConnectCnt + " / userID=" + userId); 
+    }
+    else {
+        // 인자없음
+        // 호출사례  http://myserver.com/?userId=1001&userName=James
+        res.render("main8000", {
+            title: "Service Home",
+            ctime: currTime,
+            totalcnt : totalConnectCnt,
+        });
 
-      console.log("C1-A. Service WebPage HOME Time=" + currTime + " / Count=" + totalConnectCnt + " / userID=" + userId); 
-  }
-  else {
-      // 인자없음
-      // 호출사례  http://myserver.com/?userId=1001&userName=James
-      res.render("main8000", {
-          title: "Service Home",
-          ctime: currTime,
-          totalcnt : totalConnectCnt,
-      });
-
-      console.log("C1-B. Service WebPage HOME Time=" + currTime + " / Count=" + totalConnectCnt); 
+        console.log("C1-B. Service WebPage HOME Time=" + currTime + " / Count=" + totalConnectCnt); 
     }; 
-
-
  
 });
-
  
-
 app.listen(PORT, () => {
-  console.log('Http V2.03 is running at:', PORT);
+  console.log('HTTP V2.04 is running at:', PORT);
 });
