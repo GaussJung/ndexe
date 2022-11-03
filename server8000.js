38390d5cc42d8acead6f067359bf9a5a94c68567
ColinJung <master@entercube.net>
2022-11-03 21:43:05 +0900

    v1.78

--- server8000.js
+++ server8000.js
@@ -1,6 +1,4 @@
-﻿
-
-/*
+﻿/*
 - 프로그램 : server8000.js 
 - Version : 2.01 
 - Date : 2022. 04. 01  
@@ -31,23 +29,44 @@ app.get('/', (req, res) => {
   // 접속시간 정보 설정 
   let todayDate = new Date(); 
   let currTime = todayDate.toFormat('YYYY-MM-DD HH24:MI:SS');
- 
+
   // 접속횟수 추가 
   totalConnectCnt++; 
-
-  // 렌더링 
-  res.render("main8000", {
-      title: "Service Home",
-      ctime: currTime,
-      totalcnt : totalConnectCnt
-  });
  
-  console.log("Service WebPage HOME Time=" + currTime + " / Count=" + totalConnectCnt); 
+  // 인자값 전달유무에 따른 페이지 분리 
+  if ( req.query != undefined && req.query !== null ) {
+      // 인자있음 
+      // 호출사례  http://myserver.com/?userId=1001&userName=James
+      let {userId, userName} = req.query;
+ 
+      res.render("main8010", {
+          title: "Member Home",
+          ctime: currTime,
+          totalcnt : totalConnectCnt,
+          uid : userId,
+          uname : userName,
+      });
 
+      console.log("C1-A. Service WebPage HOME Time=" + currTime + " / Count=" + totalConnectCnt + " / userID=" + userId); 
+  }
+  else {
+      // 인자없음
+      // 호출사례  http://myserver.com/?userId=1001&userName=James
+      res.render("main8000", {
+          title: "Service Home",
+          ctime: currTime,
+          totalcnt : totalConnectCnt,
+      });
+
+      console.log("C1-B. Service WebPage HOME Time=" + currTime + " / Count=" + totalConnectCnt); 
+    }; 
+
+
+ 
 });
 
  
 
 app.listen(PORT, () => {
-  console.log('Node + Nginx WebServer V2.01 is running at:', PORT);
+  console.log('Http V2.03 is running at:', PORT);
 });
